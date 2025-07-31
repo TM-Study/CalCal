5dd8c950ea9528263aa961be1a31fc5ec86c8e79
TM-Study <pveriow2@gmai.com>
2025-07-31 22:25:43 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ const Home: React.FC = () => {
         },
         body: JSON.stringify({ date, item, cal }),
       });
+      handleClose();
     } catch (error) {
       alert('登録に失敗しました');
       throw error;
@@ -38,7 +39,7 @@ const Home: React.FC = () => {
       {/* 登録ダイアログ */}
       <RegisterDialog
         open={open}
-        handleClose={handleClose}
+        handleClose={() => {}}
         handleRegister={handleRegister}
       />
     </div>
